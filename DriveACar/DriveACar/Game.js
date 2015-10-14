68b73635b76f717a3adc9c3d35503eb00db1ef43
hook25 <massigi@live.it>
2015-10-14 17:18:29 +0200

    added canvas and started game js

--- DriveACar/DriveACar/Game.js
+++ DriveACar/DriveACar/Game.js
@@ -1,1 +1,16 @@
-﻿
+﻿var canvas, ctx, scene, camera, renderer;
+function init() {
+    canvas = document.getElementById("cv");
+    ctx = canvas.getContext("2d");
+    scene = new THREE.Scene(); //God
+    camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 1000);
+    //camera... nothing more
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(canvas.width, canvas.height);
+    document.body.appendChild(renderer.domElement);
+}
+function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
+render(); //renderizza
