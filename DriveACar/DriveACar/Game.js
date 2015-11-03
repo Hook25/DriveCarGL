857174d60101fa1e623f4d18a1f939507852c11a
hook25 <massigi@live.it>
2015-11-03 16:48:37 +0100

    Sync with drive, GG marconi proxy issue

--- DriveACar/DriveACar/Game.js
+++ DriveACar/DriveACar/Game.js
@@ -1,4 +1,4 @@
-﻿var canvas, ctx, scene, camera, renderer;
+﻿var canvas, ctx, scene, camera, renderer, car;
 function init() {
     canvas = document.getElementById("cv");
     ctx = canvas.getContext("2d");
@@ -8,9 +8,31 @@ function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(canvas.width, canvas.height);
     document.body.appendChild(renderer.domElement);
+    load_model();
+    render();
+}
+function load_model() {
+    var loader = new THREE.ObjectLoader();
+    loader.load(
+            'Cube/cuboDIM3.obj',
+            function (obj) {
+                scene.add(obj);
+            }
+        )
 }
 function render() {
     requestAnimationFrame(render);
     renderer.render(scene, camera);
 }
-render(); //renderizza
+function move_car(e) {
+    if (e == 'w') {
+        car.move_forward();
+    } else if (e == 'a') {
+        car.move_left();
+    } else if (e == 's') {
+        car.move_right();
+    } else if (e == 'd') {
+        car.slow_down();
+    }
+
+}
---
+++ PseudoCompiler/PseudoCompiler/Command.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace PseudoCode {
+    public class Command:IEquatable<string> {
+        public string PseudoCommand { get; set; }
+        public string TrasformationRule { get; set; } 
+        ///the name of the action to be executed, reflecting action can
+        ///be an security issue, to be changed ?  TODO
+        public string EffectiveCommand { get; set; }
+        private object[] _trasformationSupplements { get; set; }
+        public Command(string _pseudoCommand, string _effCommand, string _a = "", object[] _args = null) {
+            this.PseudoCommand = _pseudoCommand; this.EffectiveCommand = _effCommand; 
+            this.TrasformationRule = _a; this._trasformationSupplements = _args;
+        }
+        public Command() { }
+        public bool Equals(string s) {
+            return PseudoCommand.Equals(s);
+        }
+    }
+    public class Factory {
+        List<Command> lsc { get; set; }
+        public Factory() {
+            lsc = new List<Command>();
+        }
+        public void SaveFactory() {
+            var serializer = new XmlSerializer(typeof(List<Command>));
+            serializer.Serialize(new StreamWriter("C:\\Lavoro-temp\\saves.xml"), lsc);
+        }
+        public void AddCommand(Command c) {
+            lsc.Add(c);
+        }
+        public void GetCommand(string c) {
+            return lsc.Find((st)=>{
+                return st.PseudoCommand == c;
+            }
+                )
+        }
+    }
+}
