7e5a57f3d57a0a0a03a11828edc0beb04c128a72
hook25 <massigi@live.it>
2015-11-12 21:05:28 +0100

    Working on car movment

---
+++ DriveACar/DriveACar/devJS.js
@@ -0,0 +1,129 @@
+// JavaScript source code
+render_time = 1;
+var container;
+
+var car;
+
+var camera, scene, renderer;
+
+var mouseX = 0, mouseY = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+
+init();
+animate();
+
+
+function init() {
+
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.z = 200;
+
+    // scene
+
+    scene = new THREE.Scene();
+
+    var ambient = new THREE.AmbientLight(0x101030);
+    scene.add(ambient);
+
+    var directionalLight = new THREE.DirectionalLight(0xffeedd);
+    directionalLight.position.set(0, 0, 1);
+    scene.add(directionalLight);
+    // model
+    var loader = new THREE.OBJLoader();
+    loader.load('cube/cuboDIM2.obj', function (object) {
+
+        object.traverse(function (child) {
+        });
+
+        object.position.y = -80;
+        car = car_factory(200, object);
+        scene.add(car.model)
+    });
+    // end model
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+    document.addEventListener('keydown', move_car, false);
+    window.addEventListener('resize', onWindowResize, false);
+}
+function onWindowResize() {
+
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+function onDocumentMouseMove(event) {
+
+    mouseX = (event.clientX - windowHalfX) / 2;
+    mouseY = (event.clientY - windowHalfY) / 2;
+
+}
+function animate() {
+
+    requestAnimationFrame(animate);
+    render();
+
+}
+function render() {
+
+    camera.position.x += (mouseX - camera.position.x) * .05;
+    camera.position.y += (-mouseY - camera.position.y) * .05;
+
+    camera.lookAt(scene.position);
+
+    renderer.render(scene, camera);
+    car.model.position.x += car.speed;
+    car.model.position.y += car.speed;
+}
+function car_factory(maxspeed, model) {
+    var car = {
+        max_speed: maxspeed,
+        model: model,
+        speed: 0,
+        how_right: 0, //if this is set to 0 the car will just go straight
+        move_forward: move_forwardP,
+        slow_down: move_slow,
+        accelleration: 0.25,
+    }
+    return car;
+}
+function move_forwardP() {
+    //speed = actualSpeed + a*renderTime(seconds)
+    this.speed = this.speed + this.accelleration * render_time;
+}
+function move_slow() {
+    this.speed = this.speed - this.accelleration * render_time;
+}
+function move_right() {
+    //first design of turning, how the car is turned is defined by a number
+    //going from -1 to 1
+    this.how_right += 0.1;
+    if (this.how_right >= 0.9) { this.how_right = -1; console.log("Turned of 180°");}
+}
+function move_left() {
+    this.speed = this.speed + this.accelleration * render_time;
+}
+function move_car(e) {
+    if (e.keyCode == "W".charCodeAt(0) ) {
+        car.move_forward();
+    } else if (e.keyCode == 'A'.charCodeAt(0)) {
+        car.move_left();
+    } else if (e.keyCode == 'D'.charCodeAt(0)) {
+        car.move_right();
+    } else if (e.keyCode == 'S'.charCodeAt(0)) {
+        car.slow_down();
+    }
+}
